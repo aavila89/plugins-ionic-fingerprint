ed52f23373b0aea0efa58b1f40f8077586fa677f
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-28 23:42:55 -0400

    feat: Implemented FingerprintAIO plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {FingerprintAIO} from '@awesome-cordova-plugins/fingerprint-aio/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FingerprintAIO],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FingerprintAIO, FingerprintOptions} from '@awesome-cordova-plugins/fingerprint-aio/ngx';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  fpo!: FingerprintOptions;
+  available!: string;
 
-  constructor() {}
+  constructor(
+    private fp: FingerprintAIO,
+    private alertCtrl: AlertController,
+    private platform: Platform
+  ) {}
+
+  ngOnInit(): void {
+    this.fpo = {
+      title: 'Title Fingerprint', //(android only)
+      cancelButtonTitle: 'Cancel Button', //(android only)
+      disableBackup: true
+    };
+
+    this.platform.ready().then(() => {
+      this.fp.isAvailable().then((result) => {
+        this.available = result;
+        console.log({result});
+      }).catch((err) => {
+        console.log({err});
+      });
+    });
+  }
+
+  onClick() {
+    if (this.available === 'finger' || this.available === 'face') {
+      //@ts-ignore
+      this.fp.show(this.fpo).then((success: string) => {
+          // Fingerprint/Face was successfully verified
+         this.presentAlert('', success);
+      }).catch((error: any) => {
+          // Fingerprint/Face was not successfully verified
+          this.presentAlert('', error.message);
+      });
+    }
+  }
+
+  async presentAlert(title: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
 
 }
